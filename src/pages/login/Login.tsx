541a989e75656bdeb3da0d53b231aa5e74a25448
AntoineZK <antoine.khalidy@gmail.com>
2024-10-24 15:55:46 +0900

    add login page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,18 +1,42 @@
-import { useState } from 'react'
-import reactLogo from '../../assets/react.svg'
-import viteLogo from '/vite.svg'
-import './Login.css'
+import './Login.css';
 
-function Login() {
-  const [count, setCount] = useState(0)
-
+const Login = () => {
   return (
     <>
-      <h1>
-        Login Page
-      </h1>
+      <div className="login-container">
+        <div className="welcome-section">
+          <h1>Welcome Back!</h1>
+        </div>
+        <div className="login-section">
+          <div className="login-header">
+            <h1>Login</h1>
+            <p>Welcome back! Please login to your account.</p>
+          </div>
+          <form className='login-form'>
+            <label htmlFor="username">User Name</label>
+            <input type="text" id="username" placeholder="username@gmail.com" />
+
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" placeholder="********" />
+
+            <div className="remember-forgot">
+              <label>
+                <input type="checkbox" /> Remember Me
+              </label>
+              <a href="#">Forgot Password?</a>
+            </div>
+
+            <button className="login-button" type="submit">Login</button>
+          </form>
+
+
+          <div className="login-footer">
+            New User? <a href="#">Signup</a>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Login
+export default Login;
